53f59ab37e39969c693da98228840de51a00a8fe
ARCODA\manuel.tardivo <manuel.tardivo@arcoda.it>
2021-11-05 13:25:54 +0100

    feat(login) exceptions managed

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthGuardService } from 'src/app/shared/services/auth-guard/auth-guard.service';
 import { DatabaseService } from 'src/app/shared/services/database/database.service';
+import { SlaveService } from 'src/app/shared/services/slave/slave.service';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
   public email: string = "";
   public password: string = "";
   public isPasswordVisible: boolean = false;
-  constructor(private _authGuardService: AuthGuardService, private _router: Router, private _db: DatabaseService) {
+  constructor(private _authGuardService: AuthGuardService, private _router: Router, private _db: DatabaseService, private _slave: SlaveService) {
   }
 
   async ngOnInit(): Promise<void> {
@@ -29,7 +30,8 @@ export class LoginComponent implements OnInit {
         this._router.navigate(['/homepage'])
       },
       error => {
-        console.log(error)
+        console.log(error);
+        this._slave.showSnackbar({ title: "Errore!", subtitle: error?.message, autoDestroySeconds: 3 })
       }
     )
   }
